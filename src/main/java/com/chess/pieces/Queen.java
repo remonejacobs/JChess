2c5083f2c58d21c41b60b33f676588b7c6e5d730
rejacobcpt024 <rejacobcpt024@student.wethinkcode.co.za>
2025-06-05 10:22:35 +0200

    queen moves is just combo of bishop and rook moves

--- src/main/java/com/chess/pieces/Queen.java
+++ src/main/java/com/chess/pieces/Queen.java
@@ -1,5 +1,6 @@
 package com.chess.pieces;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Queen extends Piece{
@@ -15,6 +16,116 @@ public class Queen extends Piece{
 
     @Override
     public List<Position> moves(Object[][] board) {
-        return List.of();
+        List<Position> allMoves = new ArrayList<>();
+
+        for (int i = 1; i < 8; i++) {
+            try {
+                if (board[getPosition().getY() + i][getPosition().getX() + i] != null) {
+                    Piece piece = (Piece) board[getPosition().getY() + i][getPosition().getX() + 1];
+
+                    if (!piece.getColor().equals(getColor())) {
+                        allMoves.add(piece.getPosition());
+                    }
+                    break;
+                } else {
+                    allMoves.add(new Position(getPosition().getX() + 1, getPosition().getY() + 1));
+                }
+            } catch (Exception ignored) {
+            }
+        }
+
+        for (int i = 1; i < 8; i++) {
+            try {
+                if (board[getPosition().getY() - i][getPosition().getX() - i] != null) {
+                    Piece piece = (Piece) board[getPosition().getY() - i][getPosition().getX() - 1];
+
+                    if (!piece.getColor().equals(getColor())) {
+                        allMoves.add(piece.getPosition());
+                    }
+                    break;
+                } else {
+                    allMoves.add(new Position(getPosition().getX() - 1, getPosition().getY() - 1));
+                }
+            } catch (Exception ignored) {
+            }
+        }
+
+        for (int i = 1; i < 8; i++) {
+            try {
+                if (board[getPosition().getY() + i][getPosition().getX() - i] != null) {
+                    Piece piece = (Piece) board[getPosition().getY() + i][getPosition().getX() - 1];
+
+                    if (!piece.getColor().equals(getColor())) {
+                        allMoves.add(piece.getPosition());
+                    }
+                    break;
+                } else {
+                    allMoves.add(new Position(getPosition().getX() - 1, getPosition().getY() + 1));
+                }
+            } catch (Exception ignored) {
+            }
+        }
+
+        for (int i = 1; i < 8; i++) {
+            try {
+                if (board[getPosition().getY() - i][getPosition().getX() + i] != null) {
+                    Piece piece = (Piece) board[getPosition().getY() - i][getPosition().getX() + 1];
+
+                    if (!piece.getColor().equals(getColor())) {
+                        allMoves.add(piece.getPosition());
+                    }
+                    break;
+                } else {
+                    allMoves.add(new Position(getPosition().getX() + 1, getPosition().getY() - 1));
+                }
+            } catch (Exception ignored) {
+            }
+        }
+
+        // handles all moves vertical
+        for (int a = 0; a < 2; a++) {
+            for (int i = 1; i < 8 - getPosition().getY(); i++) {
+                int add = i;
+                if (a > 0) {
+                    add *= -1;
+                }
+
+                try {
+                    if (board[getPosition().getY() + add][getPosition().getX()] != null) {
+                        Piece piece = (Piece) board[getPosition().getY() + add][getPosition().getX()];
+                        if (!piece.getColor().equals(getColor())) {
+                            allMoves.add(piece.getPosition());
+                        }
+                        break;
+                    } else {
+                        allMoves.add(new Position(getPosition().getX(), getPosition().getY() + add));
+                    }
+                } catch (Exception ignored) {
+                }
+            }
+
+            // handles all moves horizontal
+            for (int i = 1; i < 8 - getPosition().getX(); i++) {
+                int add = i;
+                if (a > 0) {
+                    add *= -1;
+                }
+                try {
+
+                    if (board[getPosition().getY()][getPosition().getX() + add] != null) {
+                        Piece piece = (Piece) board[getPosition().getY()][getPosition().getX() + add];
+                        if (!piece.getColor().equals(getColor())) {
+                            allMoves.add(piece.getPosition());
+                        }
+                        break;
+                    } else {
+                        allMoves.add(new Position(getPosition().getX() + add, getPosition().getY()));
+                    }
+                } catch (Exception ignored) {
+                }
+
+            }
+        }
+        return allMoves;
     }
 }
